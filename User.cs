c297151781d029ef6946aafd6dd8e7aad617297d
FatimaOY <r1000778@student.thomasmore.be>
2024-04-22 16:50:00 +0200

    Class user added

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitfinder
+{
+    public class Person
+    {
+        //Properties 
+        public string Name { get; set; }
+        public  string Surname { get; set; }
+
+        public int ID { get; set; }
+        public string EmailAdress { get; set; }
+        private string Password { get; set; }
+
+        public Role UserRole { get; set; }  
+
+
+
+
+    }
+
+    public enum Role
+    {
+        Client,
+        Trainer
+    }
+
+}

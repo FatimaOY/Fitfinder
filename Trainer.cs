01856ba7fd7e15bbea51c87e1942e6f9f4bb9c42
FatimaOY <r1000778@student.thomasmore.be>
2024-04-22 17:11:09 +0200

    added workouttypes class

---
+++ Trainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitfinder
+{
+    public class Trainer
+    {
+        public string Location { get; set; }
+
+        public WorkoutTypes Workout { get; set; }
+
+        public Double Price {  get; set; }
+
+        public int MyProperty { get; set; }
+
+
+    }
+}

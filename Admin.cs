eae9174d17719ab89b7f4b087902f88ab170a660
oliwveanda <oliwveanda@gmail.com>
2024-04-29 11:50:32 +0200

    added admin class

--- Admin.cs
+++ Admin.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Fitfinder
 {
-    public class Admin
+    public class Admin : User
     {
+        public int AdminId { get; set; }
+        public int PersonId { get { return UserId; } }
+
+        public Admin(int adminId, string name, string surname, string email, string password, byte[] profilePic, int genderId)
+            : base(name, surname, email, password, profilePic, genderId)
+        {
+            AdminId = adminId;
+        }
     }
+
 }
